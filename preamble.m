8f4c54c4b7e405908faca54833ee20cf1365cff0
pschmitthaeuser <paulschmitthaeuser@gmail.com>
2024-04-24 19:42:37 +0200

    Directory structure and preamble

---
+++ preamble.m
@@ -0,0 +1,37 @@
+%%% Prepare the directories and data
+% working_directory should be a string leading to AuditoryData
+% function returns 
+function preamble(working_directory)
+
+    % change working directory
+    % working_directory = strrep(matlab.desktop.editor.getActiveFilename, 'preamble.m', '');
+    cd(working_directory)
+    
+    
+    % create auditory
+    if ~exist('./auditory/', 'dir')
+           mkdir('./auditory/')
+    end
+    
+    
+    % create subfolders
+    subfolders = ["classical", "dummy", "jobs"];
+    
+    for sub = subfolders
+        path = strcat('./auditory/', sub, '/');
+        if ~exist(path, 'dir')
+               mkdir(path)
+        end
+    end
+    
+    
+    % move .img files 004 to 015 to dummy
+    try
+        for i = 4:15
+            number = sprintf('%03d', i);
+            file = sprintf('MoAEpilot/fM00223/fM00223_%s.img', number);
+            movefile(file, './auditory/dummy/');
+        end
+    catch
+        warning('Files do not exist, check if already moved.');
+    end
