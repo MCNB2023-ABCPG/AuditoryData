c40ce4f766c04c15073adb5b09cb3e473e96031c
pschmitthaeuser <paulschmitthaeuser@gmail.com>
2024-05-05 15:19:19 +0200

    add jobs

---
+++ auditory/jobs/coregister_job.m
@@ -0,0 +1,12 @@
+%-----------------------------------------------------------------------
+% Job saved on 05-May-2024 14:42:48 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.spatial.coreg.estimate.ref = {'/Users/pschm/Documents/University/mcnb/2_semester/NMDA-II/AuditoryData/MoAEpilot/fM00223/meanfM00223_016.img,1'};
+matlabbatch{1}.spm.spatial.coreg.estimate.source = {'/Users/pschm/Documents/University/mcnb/2_semester/NMDA-II/AuditoryData/MoAEpilot/sM00223/sM00223_002.img,1'};
+matlabbatch{1}.spm.spatial.coreg.estimate.other = {''};
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'nmi';
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.sep = [4 2];
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.fwhm = [7 7];
--- auditory/jobs/realign.m
+++ auditory/jobs/realign.m
@@ -1,5 +1,5 @@
 % List of open inputs
-nrun = X; % enter the number of runs here
+nrun = 1; % enter the number of runs here
 jobfile = {'/Users/pschm/Documents/University/mcnb/2_semester/NMDA-II/AuditoryData/auditory/jobs/realign_job.m'};
 jobs = repmat(jobfile, 1, nrun);
 inputs = cell(0, nrun);
